38ae4bfcdbdc1e1a2505b9e5dcef6e16cfb41f35
Kang HyoJun <admire9@gmail.com>
2013-04-05 03:55:13 +0900

    Start implement profie main

--- web/app/controllers/Profile.scala
+++ web/app/controllers/Profile.scala
@@ -2,10 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import com.mintpresso._
 
 object Profile extends Controller {
   
-  def index(url: String) = Action {
-    Ok(views.html.profile(url))
+  def index(serviceName: String, userName: String) = Action {
+    val affo: Affogato = new Affogato("cc64f8ee51c8420172a907baa81285ae", 13)
+    affo.get(_type="user", identifier=userName + "@" + serviceName).map { p => 
+      Ok(views.html.profile(userName))
+    }.getOrElse {
+      NotFound("Sorry")
+    }
   }
 }
