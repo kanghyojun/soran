867b60e32cc2180d1eca8c91f07df6527c4c1399
Kang HyoJun <admire9@gmail.com>
2013-04-11 15:28:39 +0900

    Deploy model

--- web/app/controllers/Profile.scala
+++ web/app/controllers/Profile.scala
@@ -2,30 +2,16 @@ package controllers
 
 import play.api._
 import play.api.mvc._
-import com.mintpresso._
 
-case class SoranMusic(affogatoPoint: Point, count: Long)
+import models._
+
 object Profile extends Controller {
   val supportServiceNames: List[String] = List[String]("bugs", "naverMusic")
   def index(serviceName: String, userName: String) = Action {
-    val affo: Affogato = new Affogato("cc64f8ee51c8420172a907baa81285ae", 13)
     val uIdentifier = "%s@%s".format(userName, serviceName)
 
-    affo.get(_type="user", identifier=uIdentifier).map { p => 
-      val listnedMusics = affo.get(
-        None, "user", uIdentifier, "listen", 
-        None, "music", "?").map { edgs =>
-
-        val musicIdWithPlayCount: Map[Long, Long] = edgs.groupBy(_._object.id).mapValues(_.length)
-        val musicLists: Iterable[SoranMusic] = for {
-          (k, v) <- musicIdWithPlayCount
-          p <- affo.get(k)
-        } yield SoranMusic(p, v)
-
-        musicLists
-      }.getOrElse {
-        Iterable[SoranMusic]()
-      }
+    User.findByIdentifier(uIdentifier).map { p =>  
+      val listnedMusics: Iterable[Music] = Music.findByIdentifier(uIdentifier)
 
       Ok(views.html.profile(userName, listnedMusics))
     }.getOrElse {
