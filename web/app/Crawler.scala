b46f5d1d59f05a1d6e9a359e83f7bc143c3803a4
Kang HyoJun <admire9@gmail.com>
2013-04-18 18:12:42 +0900

    Implement CrawlerActor

--- web/app/Crawler.scala
+++ web/app/Crawler.scala
@@ -1,9 +1,95 @@
 package com.soran
 
+import scala.concurrent.duration._
+import scala.concurrent._
 import scala.language.implicitConversions
+import java.util.Date
 
+import akka.actor.{Actor, Props, ActorSystem}
+import akka.actor.Actor._
 import dispatch._
 import org.w3c.dom.{Element, Node, NodeList, NamedNodeMap}
+import play.api.Logger
+
+import com.mintpresso._
+
+
+class CrawlerActor extends Actor {
+
+  import CrawlerActor._
+
+  val affogato = Affogato("240ff06dee7-f79f-423f-9684-0cedd2c13ef3", 240)
+  var lastModifiedSince = new Date()
+  var cached: List[String] = List[String]()
+
+
+  def receive = {
+    case Crawling() => {
+      Logger.debug("Crawling Started at %s".format(new Date()))
+      val n = Crawler.getNewTrackBugsIds()
+
+      if(cached != n) {
+        val added: List[String] = (n.toSet &~ cached.toSet).toList
+        for (
+          trackId <- added;
+          data: Map[String, String] <- Crawler.getBugsTrackInfo(trackId)
+          if data.isEmpty != true
+        ) {
+          val mintData =
+            """
+            {
+              "artist": "%s",
+              "artistId": "%s",
+              "albumArtist": "%s",
+              "albumTitle": "%s",
+              "albumId": "%s",
+              "title": "%s",
+              "genre": "%s",
+              "len": "%s",
+              "releaseDate": "%s"
+            }
+            """.format(
+            data("artist"),
+            data("artistId").toString(),
+            data("albumArtist"),
+            data("albumTitle"),
+            data("albumId").toString(),
+            data("title"),
+            data("genre"),
+            data("len"),
+            data("releaseDate")
+          )
+          try{
+            val p = affogato.set(
+              _type = "music",
+              identifier = data("music"),
+              data = mintData)
+            p.map { rp =>
+              Logger.debug("Point Identifier, " + rp.identifier)
+            }
+            if(p.isEmpty == true) Logger.debug("Affogato set failed. {identifier: %s, title: %s}".format(data("music"), data("title")))
+          } catch {
+            case e: Throwable => Logger.debug("mintdata - " + mintData)            
+          }
+        }
+
+        lastModifiedSince = new Date()
+        cached = n
+      } else {
+        Logger.debug("Not changed bugs track.") 
+      }
+    }
+  }
+
+}
+
+object CrawlerActor {
+  case class Crawling()
+
+  val system = ActorSystem("CrawlerActor")
+  val ref = system.actorOf(Props[CrawlerActor])
+}
+
 
 
 object Crawler {
