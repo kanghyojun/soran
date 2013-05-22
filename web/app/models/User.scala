53f69d6bd503f8eae6d25e36309d6966b2f4c231
Kang HyoJun <admire9@gmail.com>
2013-05-23 04:36:49 +0900

    Implement User.findNeighborByIdentifier

--- web/app/models/User.scala
+++ web/app/models/User.scala
@@ -8,4 +8,23 @@ object User extends SoranModel {
     affogato.get(_type="user", identifier=identifier).as[Option[Point]]
   }
 
+  def findNeighborByIdentifier(iden: String): List[Point] = {
+    val listened: List[Music] = Music.findByIdentifier(iden).toList
+    val top10Music: List[Music] = listened.sortWith((m1: Music, m2: Music) => m1.count > m2.count)
+    var userIds: List[Long] = List[Long]()
+    var me: Option[Point] = findByIdentifier(iden)
+    var a: List[(String, String)] = List[(String, String)]()
+
+    for(u <- me;
+        m <- top10Music;
+        edges <- Music.findUserByMusicIdentifier(m.affogatoPoint.identifier)
+    ) {
+      userIds = userIds ++ edges.filter(_.subject.id != u.id).map(_.subject.id)
+    }
+
+    (for {
+      i: Long <- userIds.toSet
+      p <- affogato.get(i)
+    } yield p).toList
+  }
 }
