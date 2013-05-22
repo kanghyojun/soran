344867dbbcc4216814dec148244f3302395f8e3e
Kang HyoJun <admire9@gmail.com>
2013-05-23 03:34:20 +0900

    Change string interpolation

--- web/app/controllers/Profile.scala
+++ web/app/controllers/Profile.scala
@@ -9,12 +9,11 @@ object Profile extends Controller {
   val supportServiceNames: List[String] = List[String]("bugs", "naverMusic")
   def index(serviceName: String, userName: String) = Action {
     val uIdentifier = "%s@%s".format(userName, serviceName)
-
     User.findByIdentifier(uIdentifier).map { p =>  
       val listnedMusics: Iterable[Music] = Music.findByIdentifier(uIdentifier)
       Ok(views.html.playlist(userName, listnedMusics))
     }.getOrElse {
-      NotFound("죄송합니다! uIdentifier님의 페이지를 찾을수 없습니다.")
+      NotFound(s"죄송합니다! $userName 님의 페이지를 찾을수 없습니다.")
     }
   }
 }
